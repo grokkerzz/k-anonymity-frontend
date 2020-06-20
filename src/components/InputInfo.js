3d8cf68ab963a414256ec86a3022325fab0f8479
leminh0796 <leminh0796@gmail.com>
2020-06-20 14:44:34 +0700

    Thêm button xem bảng

--- src/components/InputInfo.js
+++ src/components/InputInfo.js
@@ -11,12 +11,28 @@ function InputInfo() {
             setListCols(data.list_columns.join(' | '));
         });
     }, []);
+    function openTable() {
+        window.open('http://127.0.0.1:5000/inputtable');
+    }
+        
     return(
             <div class="container" style={{height: '310px', float: 'left'}}>
             <h5 class="text-left">Dữ liệu đầu vào:</h5>
             <p class="text-left">Số dòng: {numRows}</p>
             <p class="text-left">Số cột: {numCols}</p>
             <p class="text-left">Các cột: {listCols}</p>
+            <button
+                onClick={openTable}
+                style={{
+                    backgroundColor: 'rgb(147, 191, 226)', 
+                    color: 'rgb(255,255,255)',
+                    marginTop: '0px',
+                    float: 'left',
+                }}
+                className="btn visible"
+                >
+                  Xem bảng
+            </button>
             </div>
     );
 }
